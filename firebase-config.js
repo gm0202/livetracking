ce5a6f84fcd1b043ffe771520011bf5d06ff0dc6
Gaurang Mishra <gaurangmishra69@gmail.com>
2025-04-24 01:22:04 +0530

    Initial commit

---
+++ firebase-config.js
@@ -0,0 +1,13 @@
+// Your web app's Firebase configuration
+const firebaseConfig = {
+    apiKey: "AIzaSyBP4V9QM6aT3QjNTM5squPpPGd6EMgAWJE",
+    authDomain: "her-eab33.firebaseapp.com",
+    projectId: "her-eab33",
+    storageBucket: "her-eab33.firebasestorage.app",
+    messagingSenderId: "1051763109707",
+    appId: "1:1051763109707:web:7c5b4402c54150b9ca02ce"
+};
+
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+const db = firebase.firestore(); 
