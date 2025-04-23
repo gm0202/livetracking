dde6c05a51c9b284bd4c12157e5062cb11ce2745
Gaurang Mishra <gaurangmishra69@gmail.com>
2025-04-24 02:04:16 +0530

    third edit

--- firebase-config.js
+++ firebase-config.js
@@ -1,11 +1,11 @@
 // Your web app's Firebase configuration
 const firebaseConfig = {
-    apiKey: "AIzaSyBP4V9QM6aT3QjNTM5squPpPGd6EMgAWJE",
-    authDomain: "her-eab33.firebaseapp.com",
-    projectId: "her-eab33",
-    storageBucket: "her-eab33.firebasestorage.app",
-    messagingSenderId: "1051763109707",
-    appId: "1:1051763109707:web:7c5b4402c54150b9ca02ce"
+    apiKey: process.env.FIREBASE_API_KEY,
+    authDomain: process.env.FIREBASE_AUTH_DOMAIN,
+    projectId: process.env.FIREBASE_PROJECT_ID,
+    storageBucket: process.env.FIREBASE_STORAGE_BUCKET,
+    messagingSenderId: process.env.FIREBASE_MESSAGING_SENDER_ID,
+    appId: process.env.FIREBASE_APP_ID
 };
 
 // Initialize Firebase
